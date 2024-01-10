a9d780476f30ebfdb05ad431c603c3008b3d76a1
Vivekananda <vivekananda.work2022@gmail.com>
2024-01-10 22:26:47 +0530

    Vivek/projects (#6)
    * add: Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,16 @@
+import styles from './Projects.module.css'
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
+    </section>
+  );
+};
