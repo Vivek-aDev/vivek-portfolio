6550fbfffec96b15e8eb7cb8b1623bba60c652b6
Vivekananda <vivekananda.work2022@gmail.com>
2024-01-11 00:01:06 +0530

    add: contact (#7)

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,44 @@
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer className={styles.container} id="contact">
+      <div className={styles.text}>
+        <h2>Contact Me</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:vivekananda.work2022@gmail.com">
+            vivekananda.work2022@
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a
+            href="https://www.linkedin.com/in/vivekadev/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            vivekadev
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a
+            href="https://github.com/Vivek-aDev"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Vivek-aDev
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
