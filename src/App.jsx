9444348ab2c5a1e6d28f9bcb1fb70d2d698c0a4d
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-01-10 23:59:38 +0530

    add: contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css";
 
-import { Navbar } from "./components/navbar/navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Experience />
       <Projects />
+      <Contact />
     </div>
   );
 }
