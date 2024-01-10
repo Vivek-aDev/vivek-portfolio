196e2ed21c39adac7d9996887180dcc951deb0f0
Vivekananda <vivekananda.work2022@gmail.com>
2024-01-10 16:04:07 +0530

    add: Project section (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
